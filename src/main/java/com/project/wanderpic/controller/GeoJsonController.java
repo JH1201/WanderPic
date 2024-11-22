2334fb601d95e5bffe0c7c69bbdc74302d3b5774
JH1201 <wlgns120100@naver.com>
2024-11-22 18:00:35 +0900

    시군구 행정구역 추가

--- src/main/java/com/project/wanderpic/controller/GeoJsonController.java
+++ src/main/java/com/project/wanderpic/controller/GeoJsonController.java
@@ -16,10 +16,19 @@ public class GeoJsonController {
         this.resourceLoader = resourceLoader;
     }
 
-    @GetMapping("/api/geojson")
-    public ResponseEntity<Resource> getGeoJson() {
-        // 경로를 'static/sig.json'으로 변경
+    @GetMapping("/api/ctprvn_geojson")
+    public ResponseEntity<Resource> ctprvnGetGeoJson() {
+        // 경로를 'static/ctprvn-wgs84.json'으로 설정
         Resource resource = resourceLoader.getResource("classpath:static/ctprvn-wgs84.json");
+        return ResponseEntity.ok()
+                             .contentType(MediaType.APPLICATION_JSON)
+                             .body(resource);
+    }
+
+    @GetMapping("/api/sig_geojson")
+    public ResponseEntity<Resource> getGeoJson() {
+        // 경로를 'static/sig.json'으로 설정
+        Resource resource = resourceLoader.getResource("classpath:static/sig.shp_wgs84.json");
         return ResponseEntity.ok()
                              .contentType(MediaType.APPLICATION_JSON)
                              .body(resource);
